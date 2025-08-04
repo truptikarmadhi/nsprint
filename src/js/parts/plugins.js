b67ee69fca3283dcb1f0c7cf72efe6fcc93dee8d
Karan <118166745+karanKarmadhi@users.noreply.github.com>
2025-08-04 17:44:36 +0530

    about us page completed with footer slider

--- src/js/parts/plugins.js
+++ src/js/parts/plugins.js
@@ -1,9 +1,39 @@
 import "slick-carousel";
 
 export class Plugins {
-
   init() {
-
+    this.PagesSliders();
   }
 
+  PagesSliders() {
+    //  ===== product slider =====
+    $(".product-slider").slick({
+      dots: false,
+      infinite: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      lazyLoad: "ondemand",
+    });
+
+    //  ===== announcement-slider =====
+    $(".announcement-slider").slick({
+      dots: false,
+      infinite: true,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 0, // No delay between transitions
+      speed: 4000, // Smooth speed (in ms)
+      cssEase: "linear", // Linear easing for smooth scroll
+      lazyLoad: "ondemand",
+      centerMode: true,
+      pauseOnHover: false, // Optional (keeps moving even when hovered)
+      variableWidth:true
+    });
+  }
 }
