827e68d084fd9f25575f9f5d711940b082e07e60
krupa <krupa@karmadhi.com>
2025-08-06 10:50:16 +0530

    image-slider-section

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20489,7 +20489,34 @@ var Parts = /*#__PURE__*/function () {
   }
   parts_createClass(Parts, [{
     key: "init",
-    value: function init() {}
+    value: function init() {
+      this.ImageSlider();
+    }
+  }, {
+    key: "ImageSlider",
+    value: function ImageSlider() {
+      $('.slider').slick({
+        infinite: true,
+        speed: 4000,
+        // Duration of one full scroll (in ms)
+        autoplay: true,
+        autoplaySpeed: 0,
+        // Delay between scrolls (0 for continuous)
+        cssEase: 'linear',
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        responsive: [{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1
+          }
+        }]
+      });
+    }
   }]);
   return Parts;
 }();
--- src/js/parts/parts.js
+++ src/js/parts/parts.js
@@ -1,8 +1,29 @@
 export class Parts{
-
     init() {
-
+        this.ImageSlider();
     }
 
-
+    ImageSlider(){
+    $('.slider').slick({
+      infinite: true,
+      speed: 4000,               // Duration of one full scroll (in ms)
+      autoplay: true,
+      autoplaySpeed: 0,          // Delay between scrolls (0 for continuous)
+      cssEase: 'linear',
+      arrows: false,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: false,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+      ]
+    });
+  }
 }
