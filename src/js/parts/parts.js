8b0eb4b9f31b7fe9bb7c04860bf9d5e8113e294f
krupa <krupa@karmadhi.com>
2025-08-07 11:49:29 +0530

    FIx

--- src/js/parts/parts.js
+++ src/js/parts/parts.js
@@ -1,4 +1,4 @@
-import gsap from "gsap";
+// import gsap from "gsap";
 export class Parts {
   init() {
     this.ImageSlider();
@@ -29,66 +29,25 @@ export class Parts {
     });
   }
 
-  ScrollContent() {
-    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-    const container = document.querySelector(".our-work");
-    const list = document.querySelector(".carousel__nav");
-    const listItems = gsap.utils.toArray(".carousel__nav__item", list);
-    const slides = gsap.utils.toArray(".carousel__item");
-    const tl = gsap.timeline();
-    const myST = ScrollTrigger.create({
-      animation: tl,
-      id: "st",
-      trigger: container,
-      start: "top top",
-      end: "+=" + container.clientHeight * (slides.length - 1),
-      pin: container,
-      scrub: true,
-      snap: {
-        snapTo: 1 / (slides.length - 1)
-      },
-      markers: false
-    })
+  // ScrollContent() {
+  //   gsap.registerPlugin(ScrollTrigger);
 
-    gsap.set(slides, { xPercent: () => { return (window.innerWidth < 768 ? 125 : 0) }, yPercent: () => { return (window.innerWidth > 768 ? 125 : 0) }, scale: 0.5, opacity: 0 });
-    listItems.forEach((item, i) => {
-      item.addEventListener("click", e => {
-        e.preventDefault();
-        const percent = tl.labels[e.target.getAttribute("data-target")] / tl.totalDuration();
-        const scrollPos = myST.start + (myST.end - myST.start) * percent;
-        gsap.to(window, { duration: 2, scrollTo: scrollPos });
-      });
-
-      const previousItem = listItems[i - 1];
-      if (previousItem) {
-        tl
-          .to(item, { background: "#ed3c3c", boxShadow: '0 0 16px #ed3c3c' }, 0.5 * (i - 1))
-          .to(
-            slides[i],
-            {
-              opacity: 1,
-              yPercent: 0,
-              xPercent: 0,
-              scale: 1,
-            },
-            '<'
-          )
-          .to(previousItem, { backgroundColor: '#424b58', boxShadow: '0 0 16px transparent' }, '<')
-          .to(
-            slides[i - 1],
-            {
-              opacity: 0,
-              yPercent: () => { return (window.innerWidth > 768 ? -125 : 0) },
-              xPercent: () => { return (window.innerWidth < 768 ? -125 : 0) },
-              scale: 0.5,
-            },
-            '<'
-          ).add("our-work-" + (++i))
-      } else {
-        gsap.set(item, { background: "#ed3c3c", boxShadow: '0 0 16px #ed3c3c' });
-        gsap.to(slides[i], { yPercent: 0, xPercent: 0, opacity: 1, scale: 1, duration: 0 }, 0);
-        tl.add("our-work-" + (++i), "+=0")
-      }
-    });
-  }
+  //   // Animate each <li> in the scrollable menu
+  //   gsap.utils.toArray(".tab-menu li").forEach((item) => {
+  //     gsap.to(item, {
+  //       scrollTrigger: {
+  //         trigger: item,
+  //         start: "top 90%", // when item enters view
+  //         end: "bottom top",
+  //         toggleActions: "play none none reverse",
+  //         scrub: false,
+  //         scroller: ".tab-menu"
+  //       },
+  //       opacity: 1,
+  //       y: 0,
+  //       duration: 0.5,
+  //       ease: "power2.out"
+  //     });
+  //   });
+  // }
 }
